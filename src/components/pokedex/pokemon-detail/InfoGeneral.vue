117d55437ce3c38abd85cf07435996d5a9897c11
ChuTrongNhan <you@example.com>
2020-04-01 17:53:01 +0700

    Finish testing API. Finish Home page UI

---
+++ src/components/pokedex/pokemon-detail/InfoGeneral.vue
@@ -0,0 +1,71 @@
+<template lang="html">
+  <div class="general">
+    <div class="general--upper">
+      <img
+        v-bind:src="pokemon.infoGeneral.imgURL"
+        alt="No image loaded"
+        height="255"
+        width="255"
+      />
+      <div class="pkm--name">{{ pokemon.infoGeneral.name }}</div>
+      <div class="pkm--species">{{ pokemon.infoGeneral.genera }}</div>
+      <div class="pkm--types">
+        <div
+          v-for="type in pokemon.infoGeneral.types"
+          v-bind:key="type.slot"
+          class="pkm--types--tag"
+        >
+          {{ type.type.name }}
+        </div>
+      </div>
+    </div>
+    <div class="general--bottom">
+      <div class="general--bottom--head">
+        <span class="national-id">
+          National No.{{ pokemon.infoGeneral.id }}
+        </span>
+      </div>
+      <div class="general--bottom--body">
+        <table cellspacing="0">
+          <tr>
+            <th>abilities</th>
+            <td>
+              <div
+                v-for="ability in pokemon.infoGeneral.abilities"
+                v-bind:key="ability.slot"
+              >
+                <span>{{ ability.ability.name.replace(/-/g, " ") }}</span>
+                <span
+                  v-if="ability.is_hidden"
+                  class="hidden-ability"
+                >
+                  (hidden ability)
+                </span>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>catch rate</th>
+            <td>{{ pokemon.infoGeneral.catchRate }}</td>
+          </tr>
+        </table>
+      </div>
+      <div class="general--bottom--foot">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoGeneral",
+  props: {
+    pokemon: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./InfoGeneral.scss";
+</style>
--- src/views/pokedex/PokemonDetail.vue
+++ src/views/pokedex/PokemonDetail.vue
@@ -1,18 +1,94 @@
 <template lang="html">
   <div class="home">
     <Header v-bind:state="2"></Header>
-    dadssd {{ name }}
+    <div v-if="!loading && !error" class="container">
+      <InfoGeneral v-bind:pokemon="pokemon"></InfoGeneral>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Header from "@/components/common/Header.vue";
+import InfoGeneral from "@/components/pokedex/pokemon-detail/InfoGeneral.vue";
 
 export default {
   name: "PokemonDetail",
   components: {
-    Header
+    Header,
+    InfoGeneral
   },
-  props: { name: String }
+  props: { name: String },
+  data() {
+    return {
+      error: false,
+      loading: true,
+      pokemon: {
+        infoGeneral: {}
+      }
+    };
+  },
+  created() {
+    this.loading = true;
+    this.error = false;
+    this.getPokemonData1();
+  },
+  methods: {
+    errorHandling() {
+      // eslint-disable-next-line no-console
+      console.log("Oopps! Somethings has gone wrong!");
+    },
+
+    sortBySlot(a, b) {
+      return a.slot - b.slot;
+    },
+
+    getPokemonData2() {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon-species/" + this.name)
+        .then(response => {
+          if (response.status === 200) {
+            Object.assign(this.pokemon.infoGeneral, {
+              catchRate: response.data.capture_rate,
+              genderRate: response.data.gender_rate,
+              growthRate: response.data.growth_rate.name
+            });
+            response.data.genera.forEach(aGenera => {
+              if (aGenera.language.name === "en")
+                Object.assign(this.pokemon.infoGeneral, {
+                  genera: aGenera.genus
+                });
+            });
+          }
+        })
+        .catch(error => {
+          this.errorHandling(error);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+
+    getPokemonData1() {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/" + this.name)
+        .then(response => {
+          if (response.status === 200) {
+            Object.assign(this.pokemon.infoGeneral, {
+              id: response.data.id,
+              name: response.data.name,
+              abilities: response.data.abilities.sort(this.sortBySlot),
+              types: response.data.types.sort(this.sortBySlot),
+              stats: response.data.stats,
+              imgURL: "https://img.pokemondb.net/artwork/" + this.name + ".jpg"
+            });
+            this.getPokemonData2();
+          }
+        })
+        .catch(error => {
+          this.errorHandling(error);
+        });
+    }
+  }
 };
 </script>
